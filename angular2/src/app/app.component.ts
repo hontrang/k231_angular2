05f3161f555aae77281b03c351e5f7697c641404
hontrang <trang11392@gmail.com>
2017-05-31 00:38:44 +0700

    moved jquery code to: angular2\src\app\modules\mol-slider\mol-slider.component.ts angular2\src\app\modules\mol-main-content\mol-main-content.component.ts
    Signed-off-by: hontrang <trang11392@gmail.com>

--- angular2/src/app/app.component.ts
+++ angular2/src/app/app.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import * as $ from 'jquery';
+
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: './app.component.html',
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent implements OnInit {
+  name = 'Angular';
+  constructor() {
+
+  }
+  ngOnInit() {
+  }
+}
--- angular2/src/app/app.module.ts
+++ angular2/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { MolSliderComponent }  from './modules/mol-slider/mol-slider.component';
+import {MolMainContentComponent} from './modules/mol-main-content/mol-main-content.component'
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, MolSliderComponent, MolMainContentComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }

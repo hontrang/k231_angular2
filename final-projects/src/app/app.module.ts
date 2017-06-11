77b1a832b07e2a920c9caf35f2ec7e715e6daec3
hontrang <trang11392@gmail.com>
2017-06-11 20:24:57 +0700

    commit service and routing
    Signed-off-by: hontrang <trang11392@gmail.com>

--- final-projects/src/app/app.module.ts
+++ final-projects/src/app/app.module.ts
@@ -25,17 +25,12 @@ import { PromoComponent } from "./components/promo/promo.component";
 import { SliderComponent } from "./components/slider/slider.component";
 import { ProductBigTitleComponent } from "./components/product-big-title/product-big-title.component";
 import { SingleShopProductComponent } from "./components/single-shop-product/single-shop-product.component";
-import {SingleProductLeftComponent} from "./components/single-product-left/single-product-left.component";
-import {SingleProductRightComponent} from "./components/single-product-right/single-product-right.component";
-
 
 //Modules
 import { HomeContentComponent } from "./module/home-page/home-content.component";
 import { SignUpComponent } from "./module/sign-up-page/sign-up.component";
 import { LoginPageComponent } from "./module/login-page/login-page.component";
 import { SingleShopProductContentComponent } from "./module/single-shop-product-page/single-shop-product-content.component";
-import { SingleProductContentComponent } from "./module/single-product-page/single-product-content.component";
-//Service
 
 
 //Service
@@ -43,14 +38,13 @@ import {ProductService} from './services/product.service';
 import {UserService} from './services/user.service';
 import {EventSubscribeService} from './services/refesh-event.service'
 
-import { SingleProductLeftService } from "./services/single-product-left-service.service";
 @NgModule({
   imports:      [ BrowserModule,FormsModule,routedComponents ],
   declarations: [ AppComponent, HeaderComponent, MainmenuComponent, SiteBrandingComponent, FooterTopComponent, FooterBottomComponent,
                   BrandsComponent, MaincontentComponent, ProductWidgetComponent, PromoComponent, SliderComponent,
                   ProductBigTitleComponent, SingleShopProductComponent,
-                  HomeContentComponent, SingleShopProductContentComponent,ChatComponent,SignUpComponent,LoginPageComponent,SingleProductLeftComponent,SingleProductRightComponent,SingleProductContentComponent],
+                  HomeContentComponent, SingleShopProductContentComponent,ChatComponent,SignUpComponent,LoginPageComponent],
   bootstrap:    [ AppComponent ],
-  providers: [ProductService,UserService,EventSubscribeService,SingleProductLeftService]
+  providers: [ProductService,UserService,EventSubscribeService]
 })
 export class AppModule { }

315cbc369f544c854831f139cc69768e4e4b06d7
secretmanpro <secretmanpro@gmail.com>
2017-06-13 22:40:05 +0700

    commit folder models

---
+++ final-projects/src/app/models/mock_product.js
@@ -0,0 +1,18 @@
+"use strict";
+var product_1 = require("./product");
+exports.ListProduct = [
+    new product_1.Product(1, "Samsung Galaxy s5- 2015", "700.00", "100.00", "product-1.jpg"),
+    new product_1.Product(2, "Nokia Lumia 1320", "899.00", "999.00", "product-2.jpg"),
+    new product_1.Product(3, "LG Leon 2015", "400.00", "425.00", "product-3.jpg"),
+    new product_1.Product(4, "Sony microsoft", "200.00", "", "product-4.jpg"),
+    new product_1.Product(5, "iPhone 6", "1200.00", "1355.00", "product-5.jpg"),
+    new product_1.Product(6, "iPhone 6", "1200.00", "1355.00", "product-5.jpg"),
+    new product_1.Product(7, "Samsung gallaxy note 4", "900.00", "", "product-6.jpg"),
+    new product_1.Product(8, "Samsung gallaxy note 5", "500.00", "", "product-1.jpg"),
+    new product_1.Product(9, "Samsung gallaxy note 6", "300.00", "", "product-5.jpg"),
+    new product_1.Product(10, "Samsung gallaxy note 7", "50.00", "", "product-4.jpg"),
+    new product_1.Product(11, "Samsung gallaxy note 8", "100.00", "", "product-3.jpg"),
+    new product_1.Product(12, "Samsung gallaxy note 9", "400.00", "", "product-2.jpg"),
+    new product_1.Product(13, "Samsung gallaxy note 10", "200.00", "", "product-1.jpg"),
+];
+//# sourceMappingURL=mock_product.js.map
---
+++ final-projects/src/app/models/mock_topnewproducts.js
@@ -0,0 +1,8 @@
+"use strict";
+var product_1 = require("./product");
+exports.TopNewProduct = [
+    new product_1.Product(10, "Apple new i phone 6", "400.00", "500.00", "product-thumb-3.jpg"),
+    new product_1.Product(11, "Samsung gallaxy note 3", "475.00", "359.00", "product-thumb-2.jpg"),
+    new product_1.Product(12, "Ipad Air 5", "540.00", "456.00", "product-thumb-1.jpg")
+];
+//# sourceMappingURL=mock_topnewproducts.js.map

abfed631db6de4c82183355c5af07dda1834fbc5
secretmanpro <secretmanpro@gmail.com>
2017-06-20 16:29:24 +0700

    add contect page

---
+++ final-projects/src/app/module/contact-page/contact-page-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+
+
+
+@Component({
+    selector: 'contact-page-content',
+    templateUrl: './contact-page-content.component.html',
+   
+})
+export class ContactPageContentComponent implements OnInit {
+    constructor() { }
+    ngOnInit() { }
+
+}
